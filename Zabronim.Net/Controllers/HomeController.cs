c97075554d4b501c23fa0373279bf40d3fba9b6d
Sardar <prosardar@ya.ru>
2014-02-02 21:00:44 +0600

    Валидация ввода почты

--- Zabronim.Net/Controllers/HomeController.cs
+++ Zabronim.Net/Controllers/HomeController.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zabronim.Net.Models;
 
-namespace Zabronim.Net.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Zabronim.Net.Controllers {
+    public class HomeController : Controller {
         //
         // GET: /Home/
-
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SumbitEmail(WClient client) {
+            if (client == null || ModelState.IsValid == false) {
+                return View("Index");
+            }
+            
+            //db.Users.Add(user);
+
+            try {
+                //  db.SaveChanges();
+            }
+            catch (Exception e) {
+                ModelState.AddModelError("Email", e);
+                return View("Index");
+            }
+            return Redirect("Index");
+        }
+
+        public ActionResult G() {
+            return View();
+        }
     }
 }
---
+++ Zabronim.Net/Models/WClient.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zabronim.Net.Models {
+    public class WClient {
+        [Required(ErrorMessage = "Введите адрес эл. почты")]
+        [EmailAddressAttribute]
+        public string Email { get; set; }
+    }
+}
