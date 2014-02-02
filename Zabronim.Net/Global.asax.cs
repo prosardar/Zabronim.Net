743ff83df9531d555af17739873302b6596ba204
Sardar <prosardar@ya.ru>
2014-02-02 21:59:53 +0600

    Добавлен dbMigration

--- Zabronim.Net/Global.asax.cs
+++ Zabronim.Net/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System.Web;
+﻿using System.Data.Entity.Migrations;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Zabronim.Net.Migrations;
 
 namespace Zabronim.Net {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -10,6 +12,8 @@ namespace Zabronim.Net {
 
     public class MvcApplication : HttpApplication {
         protected void Application_Start() {
+            new DbMigrator(new Configuration()).Update();
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
