72e74a046dc33ae8622e1527788b1bb2b094cde9
Sardar <prosardar@ya.ru>
2014-02-07 14:22:09 +0600

    Параметры почты вставлены в web.config

---
+++ Zabronim.Net/Models/EmailManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Web.Helpers;
+
+namespace Zabronim.Net.Models {
+    public class EmailManager {
+        public static void Send(string recipientEmail, string subject, string body) {
+            var mailSection = (System.Net.Configuration.SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+            var network = mailSection.Network;
+
+            WebMail.From = mailSection.From;
+            WebMail.SmtpServer = network.Host;
+            WebMail.UserName = network.UserName;
+            WebMail.Password = network.Password;
+            WebMail.SmtpPort = network.Port;
+            WebMail.SmtpUseDefaultCredentials = network.DefaultCredentials;
+
+            WebMail.Send(recipientEmail, subject, body);
+        }
+    }
+}
