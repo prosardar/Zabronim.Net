748b62043c7c69301dbb162546e6882008f93799
Sardar <prosardar@ya.ru>
2014-02-10 13:26:29 +0600

    Добавлен новый маршрут

--- Zabronim.Net/App_Start/WebApiConfig.cs
+++ Zabronim.Net/App_Start/WebApiConfig.cs
@@ -11,6 +11,12 @@ namespace Zabronim.Net {
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
