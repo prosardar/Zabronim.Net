be0fa0ca23ab5c918965780ba6af996f7fcf9956
Sardar <prosardar@ya.ru>
2014-03-13 11:36:52 +0600

    указана одна база
    и база заполнена различными процедурами

--- Zabronim.Net/Global.asax.cs
+++ Zabronim.Net/Global.asax.cs
@@ -4,15 +4,19 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 using NLog;
 using Zabronim.Net.Migrations;
+using Zabronim.Net.Models;
 
 namespace Zabronim.Net {
     public class MvcApplication : HttpApplication {
-        public static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public static readonly ZLogger Logger = new ZLogger();
 
         protected void Application_Start() {
-            Logger.Info("Application Start");
+            InitMembersDb();
+
+            ZLogger.Info("Application Start");
 
             new DbMigrator(new Configuration()).Update();
 
@@ -22,6 +26,43 @@ namespace Zabronim.Net {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void InitMembersDb() {
+            // Create some arrays for membership and role data
+            string[] users = { "Alice", "Bob", "Charlie" };
+            string[] emails = { "alice@example.org", "bob@example.org", "charlie@example.org" };
+            string[] passwords = { "ecilA-123", "treboR-456", "eilrahC-789" };
+            string[] roles = { "Super Users", "Registered Users", "Users" };
+
+            // Clear out existing user information and add fresh user information
+            for (int i = 0; i < emails.Length; i++) {
+                if (Membership.GetUserNameByEmail(emails[i]) != null)
+                    Membership.DeleteUser(users[i], true);
+
+                Membership.CreateUser(users[i], passwords[i], emails[i]);
+            }
+
+            // Clear out existing role information and add fresh role information
+            // This puts Alice, Bob and Charlie in the Users Role, Alice and Bob 
+            // in the Registered Users Role and just Alice in the Super Users Role.
+            for (int i = 0; i < roles.Length; i++) {
+                if (Roles.RoleExists(roles[i])) {
+                    foreach (string u in Roles.GetUsersInRole(roles[i]))
+                        Roles.RemoveUserFromRole(u, roles[i]);
+
+                    Roles.DeleteRole(roles[i]);
+                }
+
+                Roles.CreateRole(roles[i]);
+
+                string[] userstoadd = new string[i + 1];
+
+                for (int j = 0; j < userstoadd.Length; j++)
+                    userstoadd[j] = users[j];
+
+                Roles.AddUsersToRole(userstoadd, roles[i]);
+            }
         }
     }
 }
---
+++ Zabronim.Net/Models/Wcfц/Contracts/ErpModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Permissions;
+using System.ServiceModel;
+
+namespace Zabronim.Net.Models.Wcf.Contracts {
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession, ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class ErpModule : IErpModule {
+        readonly IErpModuleCallback callback;
+
+        public ErpModule() {
+            callback = OperationContext.Current.GetCallbackChannel<IErpModuleCallback>();
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "Manager")]
+        public string GetConnectionId(string erpClientId) {
+            Console.WriteLine("Callback = {0}", callback.SetRequest("1"));
+            return "1";
+        }
+    }
+
+    public class ErpModuleCallbackHandler : IErpModuleCallback {
+
+        public string SetRequest(string request) {
+            return "2";
+        }
+    }
+}
